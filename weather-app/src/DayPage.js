a59bebda20da42ecb8c5071409b42205892e1aca
undefined8 <dzamilatskiy2019@bklawtech.com>
2018-02-26 23:19:15 +0000

    5 day page

---
+++ weather-app/src/DayPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class DayPage extends React.Component {
+    render() {
+        return (
+            <div>DayPage</div>
+        );
+    }
+}
+
+export default DayPage;
